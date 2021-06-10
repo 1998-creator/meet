fdeefcc82f650c644b354a7a6b8a8196777a1007
Payton  Provo <paytonprovo1@gmail.com>
2021-06-09 20:07:51 -0400

    updating manifest

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,49 +1,61 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
-describe('show/hide an event details', () => {
+describe("show/hide an event details", () => {
   let browser;
   let page;
+  jest.setTimeout(30000);
+
   beforeAll(async () => {
-    jest.setTimeout(30000);
+    // browser = await puppeteer.launch();
     browser = await puppeteer.launch({
       headless: false,
       slowMo: 250,
-      ignoreDefaultArgs: ['--disable-extensions']
+      ignoreDefaultArgs: ["--disable-extensions"],
     });
+
     page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
   });
 
   afterAll(() => {
     browser.close();
   });
 
-  test('An event element is collapsed by default', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  test("An event element is collapsed by default", async () => {
+    const eventDetails = await page.$(".event .detailInfo");
+    expect(eventDetails).toBeNull();
+  });
+
+  test("User can expand an event to see its details", async () => {
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .detailInfo");
+    expect(eventDetails).toBeDefined();
+  });
+
+  test("User can collapse an event to hide its details", async () => {
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .detailInfo");
+    expect(eventDetails).toBeNull();
+  });
+});
+
+describe('Filter events by city', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
-    const extra = await page.$('.event .event__Details');
-
-    expect(extra).toBeNull();
   });
 
-  test('User can expand an event to see its details', async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
-    await page.click('.event .details-btn');
-
-    const extra = await page.$('.event .extra');
-    expect(extra).toBeDefined();
+  afterAll(() => {
+    browser.close();
   });
 
-  test('User can collapse an event to hide its details', async () => {
-    await page.click('.event .details-btn');
-
-    const extra = await page.$('.event .extra');
-    expect(extra).toBeNull();
+  test('When user hasn’t searched for a city, show upcoming events from all cities', async () => {
+    const events = await page.$$('.event');
+    expect(events).toHaveLength(10);
   });
-})
+});
